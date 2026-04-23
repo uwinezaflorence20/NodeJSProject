8c6cd3f73ac40b415b2ae28e8227c639d8fd098d
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-23 17:16:56 +0200

    done

--- module.js
+++ module.js
@@ -1,2 +1,8 @@
 const moduImport = require('./person')
-// console.log(moduImport)
+const {people,age} = require('./person');// using destruction
+console.log(moduImport)
+console.log(moduImport.people,moduImport.ages)
+
+// build in object that have the information on the operating system
+const os = require('os');
+console.log(os.platform,os.homedir);
--- person.js
+++ person.js
@@ -1,2 +1,8 @@
 let people =["uwineza","florence","Nyirishema"]
-console.log(people)
+const ages =[1,2,45,78,90,23]
+console.log(people);
+// module.export = "Hello";
+// module.exports = people; exporting single thing
+module.exports = {
+    people,ages
+}

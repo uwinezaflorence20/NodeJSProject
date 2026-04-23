380f4b698d9c35add8e7d2544303353a3fa885d0
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-23 16:37:32 +0200

    done well

--- global.js
+++ global.js
@@ -1,1 +1,5 @@
-// in node we get the access to the global object 
+// in node we get the access to the global object
+console.log(global) 
+global.setTimeout(() => {
+    console.log('in the timeout')
+}, 3000);

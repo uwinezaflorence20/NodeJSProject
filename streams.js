f55b81848106867917453ce0bcbde186555b579e
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-27 18:36:20 +0200

    done using the pipe

--- streams.js
+++ streams.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const readStrearm = fs.createReadStream('./docs/blog3.txt',{encoding:'utf8'});
 const writeStrearm = fs.createWriteStream('./docs/blog4.txt',{encoding:'utf8'})
-readStrearm.on('data',(chunk)=>{
-    console.log('---------------- NEW CHUNKS------------')
-    // console.log(chunk.toString());
-    console.log(chunk);
-    writeStrearm.write('\nNEW CHUNK\n');
-    writeStrearm.write(chunk)
+// readStrearm.on('data',(chunk)=>{
+//     console.log('---------------- NEW CHUNKS------------')
+//     // console.log(chunk.toString());
+//     console.log(chunk);
+//     writeStrearm.write('\nNEW CHUNK\n');
+//     writeStrearm.write(chunk)
     
-})
+// })
+
+// using piping
+readStrearm.pipe(writeStrearm);

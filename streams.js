b005ea4860d9d711d67e6838e8d8e6d4f5535d67
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-27 18:35:01 +0200

    done with write streams

--- streams.js
+++ streams.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
-const readStrearm = fs.createReadStream('./docs/blog3.txt',{encoding:'utf8'})
+const readStrearm = fs.createReadStream('./docs/blog3.txt',{encoding:'utf8'});
+const writeStrearm = fs.createWriteStream('./docs/blog4.txt',{encoding:'utf8'})
 readStrearm.on('data',(chunk)=>{
     console.log('---------------- NEW CHUNKS------------')
     // console.log(chunk.toString());
-    console.log(chunk)
+    console.log(chunk);
+    writeStrearm.write('\nNEW CHUNK\n');
+    writeStrearm.write(chunk)
+    
 })

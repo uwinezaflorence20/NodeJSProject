1996b20162e48839b129f00bdc8291b1b82dead5
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-30 18:42:21 +0200

    created a server in the nodejs

--- CreatingTheServer/server.js
+++ CreatingTheServer/server.js
@@ -1,6 +1,12 @@
 const http = require('http'); // requesting the server
-const server = http.createServer((req,res)=>{
-console.log('reqeuest made')
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+    res.setHeader("Content-Type", 'text/html');
+
+    res.write('<head><link rel ="StyleSheet" href="#"></head>')
+    res.write('<p>hello,ninjas</p>');
+     res.write('<p>hello again,ninjas</p>');
+    res.end();
 }); // creating the server
 server.listen(3000, 'localhost', () => {
     console.log("listening for request on port 3000")

4ed880939260be5b8457615992fdd89ce7b1f7a5
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-30 19:01:08 +0200

    done well

--- CreatingTheServer/server.js
+++ CreatingTheServer/server.js
@@ -4,23 +4,34 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     res.setHeader("Content-Type", 'text/html');
 
+    let path = '../views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'index.html'
+            break;
+    }
+
+
     // res.write('<head><link rel ="StyleSheet" href="#"></head>')
     // res.write('<p>hello,ninjas</p>');
     //  res.write('<p>hello again,ninjas</p>');
     // res.end();
 
     // sent an html file
-    fs.readFile('../views/index.html',(err,data)=>{
-        if(err){
+    fs.readFile('../views/index.html', (err, data) => {
+        if (err) {
             console.log(err);
             res.end();
-        }else{
+        } else {
             //res.write(data);
             res.end();
         }
 
     })
-    
+
 }); // creating the server
 server.listen(3000, 'localhost', () => {
     console.log("listening for request on port 3000")

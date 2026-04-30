cf2c18d7744af41cf1027dc077df7b7a91a80c8e
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-04-30 18:53:22 +0200

    created an index and use different method of fs to access it

--- CreatingTheServer/server.js
+++ CreatingTheServer/server.js
@@ -1,12 +1,26 @@
 const http = require('http'); // requesting the server
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     res.setHeader("Content-Type", 'text/html');
 
-    res.write('<head><link rel ="StyleSheet" href="#"></head>')
-    res.write('<p>hello,ninjas</p>');
-     res.write('<p>hello again,ninjas</p>');
-    res.end();
+    // res.write('<head><link rel ="StyleSheet" href="#"></head>')
+    // res.write('<p>hello,ninjas</p>');
+    //  res.write('<p>hello again,ninjas</p>');
+    // res.end();
+
+    // sent an html file
+    fs.readFile('../views/index.html',(err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            //res.write(data);
+            res.end();
+        }
+
+    })
+    
 }); // creating the server
 server.listen(3000, 'localhost', () => {
     console.log("listening for request on port 3000")

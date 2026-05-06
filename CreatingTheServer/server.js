a2e0221a3186fc6b64f4b3e738083e69e9cf94c0
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-05-06 14:47:23 +0200

    done with learnign about the basic of the nodejs

--- CreatingTheServer/server.js
+++ CreatingTheServer/server.js
@@ -1,41 +1,41 @@
-const http = require('http'); // requesting the server
+const http = require('http');
 const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    console.log(req.url, req.method);
     res.setHeader("Content-Type", 'text/html');
 
     let path = '../views/';
     switch (req.url) {
         case '/':
-            path += 'index.html'
+            path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
-            path += 'about.html'
+            path += 'about.html';
+            res.statusCode = 200;
             break;
+   case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location','/about')
+            break;
+
         default:
-            path += '404.html'
+            path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
-
-    // res.write('<head><link rel ="StyleSheet" href="#"></head>')
-    // res.write('<p>hello,ninjas</p>');
-    //  res.write('<p>hello again,ninjas</p>');
-    // res.end();
-
-    // sent an html file
-    fs.readFile('../views/index.html', (err, data) => {
+    fs.readFile(path, (err, data) => {  // ✅ use `path`, not hardcoded string
         if (err) {
             console.log(err);
             res.end();
         } else {
-            //res.write(data);
-            res.end();
+            res.end(data);  // ✅ send the file data
         }
+    });
+});
 
-    })
-
-}); // creating the server
 server.listen(3000, 'localhost', () => {
-    console.log("listening for request on port 3000")
-})
+    console.log("listening for request on port 3000");
+});

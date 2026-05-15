bffd855a8b3dd07c762065227eb35bf076b5f666
uwinezaflorence20 <uwinezaflorence20@gmail.com>
2026-05-15 14:14:04 +0200

    done well

--- CreatingTheServer/server.js
+++ CreatingTheServer/server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
     res.setHeader("Content-Type", 'text/html');
 
     let path = '../views/';
